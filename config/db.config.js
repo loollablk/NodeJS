f62a3de11e1a522b924dc679975c25a4390185ce
Sunimal Malkaklulage <sskmal@gmail.com>
2023-01-24 15:43:25 +0900

    initial commit

---
+++ config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "localhost",
+    PORT: 27017,
+    DB: "bezkoder_db",
+
+    URL: "mongodb+srv://loollabuser:<password>@cluster0.z32wos7.mongodb.net/?retryWrites=true&w=majority"
+};
